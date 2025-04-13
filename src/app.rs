c4d491b5d10a0f60438ee0e40a3848ac2b32ab7a
edwar4rd <edwar4rd@gmail.com>
2025-04-14 02:12:01 +0800

    refactor!: split shaders in app into separate files (with include_str macro)

--- src/app.rs
+++ src/app.rs
@@ -168,33 +168,8 @@ impl GLStuff {
             }
 
             let (vertex_shader_source, fragment_shader_source) = (
-                r#"
-                    const vec2 verts[3] = vec2[3](
-                        vec2(0.0, 1.0),
-                        vec2(-1.0, -1.0),
-                        vec2(1.0, -1.0)
-                    );
-                    const vec4 colors[3] = vec4[3](
-                        vec4(1.0, 0.0, 0.0, 1.0),
-                        vec4(0.0, 1.0, 0.0, 1.0),
-                        vec4(0.0, 0.0, 1.0, 1.0)
-                    );
-                    out vec4 v_color;
-                    uniform float u_angle;
-                    void main() {
-                        v_color = colors[gl_VertexID];
-                        gl_Position = vec4(verts[gl_VertexID], 0.0, 1.0);
-                        gl_Position.x *= cos(u_angle);
-                    }
-                "#,
-                r#"
-                    precision mediump float;
-                    in vec4 v_color;
-                    out vec4 out_color;
-                    void main() {
-                        out_color = v_color;
-                    }
-                "#,
+                include_str!("vertex.glsl"),
+                include_str!("fragment.glsl"),
             );
 
             let shader_sources = [
